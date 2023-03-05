6840a64c7c48af473aa6dfcc905a999196246457
0324bence <bence20050324@gmail.com>
2023-03-05 20:07:55 +0100

    Fix issues after testing

--- create.sql
+++ create.sql
@@ -12,7 +12,7 @@ Create Table IF not exists Hely (
 );
 
 Create Table IF not exists Halal (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     hely VARCHAR(10),
     ido DATE NOT NULL,
     ok VARCHAR(50),
@@ -30,17 +30,12 @@ Create Table IF Not exists Ember (
     foglalkozas VARCHAR(20) NOT NULL DEFAULT "Munkanélküli",
     isFerfi BOOLEAN,
     halal INT,
-    FOREIGN KEY (szül_hely) REFERENCES Hely(iranyitoszam)
+    FOREIGN KEY (szül_hely) REFERENCES Hely(iranyitoszam),
     FOREIGN KEY (halal) REFERENCES Halal(id)
 );
 
-Alter table Ember
-    ADD FOREIGN KEY (apja) REFERENCES Ember(id),
-    ADD FOREIGN KEY (anyja) REFERENCES Ember(id)
-
-
 Create Table If not exists Hazassag (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     ember1 INT NOT NULL,
     ember2 INT NOT NULL,
     hely VARCHAR(10),
@@ -48,12 +43,15 @@ Create Table If not exists Hazassag (
     FOREIGN KEY (hely) REFERENCES Hely(iranyitoszam),
     FOREIGN KEY (ember1) REFERENCES Ember(id),
     FOREIGN KEY (ember2) REFERENCES Ember(id)
-)
+);
 
 Create Table if not exists Hazassag_link (
     ember INT NOT NULL,
     hazassag INT NOT NULL,
     FOREIGN KEY (ember) REFERENCES Ember(id),
-    FOREIGN KEY (hazassag) REFERENCES Hazassag(id),
-)
+    FOREIGN KEY (hazassag) REFERENCES Hazassag(id)
+);
 
+Alter table Ember
+    ADD FOREIGN KEY (apja) REFERENCES Ember(id),
+    ADD FOREIGN KEY (anyja) REFERENCES Ember(id)
