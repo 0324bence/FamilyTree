075d4ad285d9c12281e5b70f82753212f8ab3d4f
0324bence <bence20050324@gmail.com>
2023-03-07 08:49:29 +0100

    Add semicolons to the end of queries

--- data.sql
+++ data.sql
@@ -1,56 +1,56 @@
 Insert into Orszag
-values (1, "Magyarország")
+values (1, "Magyarország");
 
 insert into Hely
-values ("6100",1,"Bács-kiskun vármegye","Kiskunfélegyháza")
+values ("6100",1,"Bács-kiskun vármegye","Kiskunfélegyháza");
 
 insert into ember (kereszt_nev, vezetek_nev, szül_hely, szül_ido, isFerfi)
-values ("József", "Doe", "6100", "1964-04-22", TRUE)
+values ("József", "Doe", "6100", "1964-04-22", TRUE);
 
 insert into ember (kereszt_nev, vezetek_nev, szül_hely, szül_ido, isFerfi)
-values ("Józsefné Katalin", "Doe", "6100", "1964-06-22", FALSE)
+values ("Józsefné Katalin", "Doe", "6100", "1964-06-22", FALSE);
 
 insert into hazassag
-values ("6100", "1885-08-10")
+values ("6100", "1885-08-10");
 
 insert into Hazassag_link
-values (1, 1)
+values (1, 1);
 
 insert into Hazassag_link
-values (2, 1)
+values (2, 1);
 
 insert into ember (kereszt_nev, vezetek_nev, szül_hely, szül_ido, apja, anyja, foglalkozas isFerfi)
-values ("II. József", "Doe", "6100", "1987-07-02", 1, 2, "Tanár", TRUE)
+values ("II. József", "Doe", "6100", "1987-07-02", 1, 2, "Tanár", TRUE);
 
 insert into ember (kereszt_nev, vezetek_nev, szül_hely, szül_ido, foglalkozas isFerfi)
-values ("Józsefné Marika", "Doe", "6100", "1988-07-02", "Tanár", FALSE)
+values ("Józsefné Marika", "Doe", "6100", "1988-07-02", "Tanár", FALSE);
 
 insert into hazassag
-values ("6100", "2004-08-10")
+values ("6100", "2004-08-10");
 
 insert into Hazassag_link
-values (3, 2)
+values (3, 2);
 
 insert into Hazassag_link
-values (4, 2)
+values (4, 2);
 
 insert into ember (kereszt_nev, vezetek_nev, szül_hely, szül_ido, apja, anyja, foglalkozas isFerfi)
-values ("Kelemen", "Doe", "6100", "1989-11-02", 1, 2, "Kőműves", TRUE)
+values ("Kelemen", "Doe", "6100", "1989-11-02", 1, 2, "Kőműves", TRUE);
 
 insert into ember (kereszt_nev, vezetek_nev, szül_hely, szül_ido, foglalkozas isFerfi)
-values ("Kelemenné", "Doe", "6100", "1991-08-17", "Beépített ember", FALSE)
+values ("Kelemenné", "Doe", "6100", "1991-08-17", "Beépített ember", FALSE);
 
 insert into hazassag
-values ("6100", "2008-08-10")
+values ("6100", "2008-08-10");
 
 insert into Hazassag_link
-values (5, 3)
+values (5, 3);
 
 insert into Hazassag_link
-values (6, 3)
+values (6, 3);
 
 insert into ember (kereszt_nev, vezetek_nev, szül_hely, szül_ido, apja, anyja, foglalkozas isFerfi)
-values ("III. József", "Doe", "6100", "2012-01-30", 3, 4,  TRUE)
+values ("III. József", "Doe", "6100", "2012-01-30", 3, 4,  TRUE);
 
 insert into ember (kereszt_nev, vezetek_nev, szül_hely, szül_ido, apja, anyja, foglalkozas isFerfi)
-values ("Kelemenke", "Doe", "6100", "2007-01-30", 5, 6,  TRUE)
+values ("Kelemenke", "Doe", "6100", "2007-01-30", 5, 6,  TRUE);
