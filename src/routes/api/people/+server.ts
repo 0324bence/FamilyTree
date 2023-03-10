5c03d5024b4d23c8aac1dc1021cff68f6c2e3a12
0324bence <bence20050324@gmail.com>
2023-03-10 09:30:25 +0100

    Try and fix get request

--- src/routes/api/people/+server.ts
+++ src/routes/api/people/+server.ts
@@ -3,7 +3,7 @@ import db from "$lib/database";
 import type { RowDataPacket } from "mysql2";
 import type { RequestHandler } from "./$types";
 
-export const GET: RequestHandler = async () => {
+export const get: RequestHandler = async () => {
     const res = (await db.query("Select * from familytree.ember")) as RowDataPacket;
     let output = "";
     for (const i of res[0]) {
