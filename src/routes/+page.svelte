09dca1fc1b5753348667cb567a5f08c42722dfe0
0324bence <bence20050324@gmail.com>
2023-03-17 15:04:17 +0100

    add modal to page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
     import type { PageData } from "./$types";
     import FamilyTree from "@balkangraph/familytree.js";
     import { onMount } from "svelte";
+    import PlaceModal from "../components/PlaceModal.svelte";
 
     type NodeType = {
         id: number;
@@ -18,6 +19,9 @@
     };
 
     export let data: PageData;
+
+    let showModal = false;
+    let currentPersonPlace = "";
 
     let container: HTMLElement;
     let nodes: NodeType[] = [];
@@ -53,6 +57,27 @@
         };
     };
 
+    FamilyTree.elements.disabledTextfield = function (data: any, editElement: any, minWidth: any, readOnly: any) {
+        var id = FamilyTree.elements.generateId();
+        var value = data[editElement.binding];
+        if (value == undefined) value = "";
+        if (readOnly && !value) {
+            return {
+                html: ""
+            };
+        }
+        return {
+            html: `<div class="bft-form-field" style="min-width: 220px;">
+                    <div class="bft-input" data-bft-input>
+                        <label for="${id}">${editElement.label}</label>
+                        <input readonly id="${id}" name="${id}" autocomplete="off" style="word-break: break-word;" data-binding="${editElement.binding}" value="${value}"/>
+                    </div>
+                </div>`,
+            id: id,
+            value: value
+        };
+    };
+
     for (let person of data[0]) {
         nodes.push({
             id: parseInt(person.id),
@@ -71,15 +96,17 @@
         });
     }
 
-    function addCountry() {
-        console.log("add country");
+    function ToggleBirthplaceEdit(person: string) {
+        console.log(person);
+        currentPersonPlace = person;
+        showModal = !showModal;
     }
 
     onMount(() => {
         document.addEventListener(
             "editBrithPlace",
-            () => {
-                addCountry();
+            (e: any) => {
+                ToggleBirthplaceEdit(e.detail);
             },
             false
         );
@@ -96,10 +123,23 @@
                     share: null,
                     pdf: null
                 },
+                addMore: undefined,
                 generateElementsFromFields: false,
                 elements: [
                     { type: "textbox", label: "Név", binding: "nev" },
-                    { type: "date", label: "Születési idő", binding: "display_date" },
+                    { type: "date", label: "Születési idő", binding: "szül_ido" },
+                    { type: "disabledTextfield", label: "Születés helye", binding: "szül_hely" },
+                    {
+                        type: "myButton",
+                        options: [
+                            {
+                                title: "Módosítás",
+                                triggerEvent: "editBrithPlace",
+                                icon: `<svg width="25" height="25" fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.8 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg>`
+                            }
+                        ],
+                        binding: "szül_hely_id"
+                    },
                     {
                         type: "select",
                         options: [
@@ -109,23 +149,15 @@
                         label: "Nem",
                         binding: "gender"
                     },
-                    { type: "textbox", label: "Foglalkozás", binding: "foglalkozas" },
-                    {
-                        type: "myButton",
-                        options: [
-                            {
-                                triggerEvent: "addNewCountry",
-                                icon: `<svg width="24" height="24" fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg>`
-                            }
-                        ]
-                    }
+                    { type: "textbox", label: "Foglalkozás", binding: "foglalkozas" }
+
                     // { type: "textbox", label: "Foglalkozás", binding: "foglalkozas" }
                 ],
                 titleBinding: "nev"
             },
             nodeBinding: {
                 field_0: "nev",
-                field_1: "szül_ido",
+                field_1: "display_date",
                 field_2: "foglalkozas"
             },
             nodeMouseClick: FamilyTree.action.edit
@@ -141,6 +173,12 @@
     {/each}
 </ul> -->
 <div id="container" bind:this={container} />
+{#if showModal}
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div id="modal-container" on:click|self={() => ToggleBirthplaceEdit("")}>
+        <PlaceModal title="Hely" current={currentPersonPlace} />
+    </div>
+{/if}
 
 <style global>
     :global(*) {
@@ -164,5 +202,16 @@
     }
     :global(.myButton:hover) {
         background-color: hsl(120, 100%, 40%);
+    }
+    #modal-container {
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100vh;
+        background-color: rgba(0, 0, 0, 0.5);
+        display: grid;
+        place-items: center;
+        z-index: 10;
     }
 </style>
